7ae621b934273d6e4705e64de7c48386a712ea11
TheObtey <95309102+TheObtey@users.noreply.github.com>
2026-03-07 22:09:00 +0100

    [ADD] LAN multiplayer.
    Add LAN multiplayer with player spawning and synchronization.

--- scripts/Player/player.gd
+++ scripts/Player/player.gd
@@ -1,15 +1,16 @@
 extends RigidBody3D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
 # Called every frame. 'iDelta' is the elapsed time since the previous frame.
 func _process(iDelta: float) -> void:
+	if multiplayer == null or not multiplayer.has_multiplayer_peer():
+		return
+	
+	if not is_multiplayer_authority():
+		return
+	
 	var vecInput := Vector3.ZERO
 	vecInput.x = Input.get_axis("move_left", "move_right")
 	vecInput.z = Input.get_axis("move_forward", "move_backward")
 	
 	apply_central_force(vecInput * 1200 * iDelta)
+	get_node("/root/Node3D/NetworkManager").send_player_transform(global_position, global_rotation)
