03fa0862982f6cbf61c8583d1cc10a4fee51ea9c
TheObtey <95309102+TheObtey@users.noreply.github.com>
2026-03-07 22:16:00 +0100

    [REFACTOR] Player script.
    Refactor player script:
    - use _physics_process method instead of _process.
    - made the player's walk speed editable through the inspector.
    - normalize vecInput to avoid speed gain when diag-walking.

--- scripts/Player/player.gd
+++ scripts/Player/player.gd
@@ -1,7 +1,9 @@
 extends RigidBody3D
 
+@export var iWalkSpeed := 1200
+
 # Called every frame. 'iDelta' is the elapsed time since the previous frame.
-func _process(iDelta: float) -> void:
+func _physics_process(iDelta: float) -> void:
 	if multiplayer == null or not multiplayer.has_multiplayer_peer():
 		return
 	
@@ -11,6 +13,7 @@ func _process(iDelta: float) -> void:
 	var vecInput := Vector3.ZERO
 	vecInput.x = Input.get_axis("move_left", "move_right")
 	vecInput.z = Input.get_axis("move_forward", "move_backward")
+	vecInput = vecInput.normalized()
 	
-	apply_central_force(vecInput * 1200 * iDelta)
+	apply_central_force(vecInput * iWalkSpeed * iDelta)
 	get_node("/root/Node3D/NetworkManager").send_player_transform(global_position, global_rotation)
