b609e88ea4eee6f27526a9f81a9047ac3603614a
E.T <ethepaut@gaming.tech>
2026-03-15 22:25:09 +0100

    [ADD] movement velocity while jumping

--- scripts/Player/MainCharacter.gd
+++ scripts/Player/MainCharacter.gd
@@ -44,13 +44,17 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * speed
-		velocity.z = direction.z * speed
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = 0.0
+			velocity.z = 0.0
 	else:
-		velocity.x = 0.0
-		velocity.z = 0.0
-
+		velocity.x = lerp(velocity.x, direction.x, delta * 7.0)
+		velocity.z = lerp(velocity.z, direction.z, delta * 7.0)
+	
 	t_sineWave += delta * velocity.length() * float(is_on_floor())
 	camera.transform.origin = _sineWave(t_sineWave)
 
