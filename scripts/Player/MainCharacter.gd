d0ac8d6905bb6873e8e2af7199c9e604e5394616
E.T <ethepaut@gaming.tech>
2026-03-15 22:20:56 +0100

    [ADD] sprint

--- scripts/Player/MainCharacter.gd
+++ scripts/Player/MainCharacter.gd
@@ -1,9 +1,12 @@
 extends CharacterBody3D
 
 #Basics Const
-const SPEED = 5.0
+const WALK_SPEED = 5.0
+const RUN_SPEED = 7.5
 const JUMP_VELOCITY = 4.5
 const MOUSE_SENSITIVITY = 0.005
+
+var speed
 
 #Sine wave Const
 const FREQUENCY = 2.0
@@ -31,14 +34,19 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
+	if Input.is_action_pressed("sprint"):
+		speed = RUN_SPEED
+	else:
+		speed = WALK_SPEED
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
 		velocity.x = 0.0
 		velocity.z = 0.0
