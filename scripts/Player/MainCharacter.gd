d2ac3d8737dc89efdf02712433798bfc24350c6b
E.T <ethepaut@gaming.tech>
2026-03-15 22:35:05 +0100

    [ADD] FOV selon speed

--- scripts/Player/MainCharacter.gd
+++ scripts/Player/MainCharacter.gd
@@ -14,6 +14,10 @@ const AMPLITUDE = 0.08
 
 var t_sineWave = 0.0
 
+#FOV const
+const BASE_FOV = 75.0
+const FOV_CHANGE = 1.5
+
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
 
@@ -57,7 +61,12 @@ func _physics_process(delta: float) -> void:
 	
 	t_sineWave += delta * velocity.length() * float(is_on_floor())
 	camera.transform.origin = _sineWave(t_sineWave)
-
+	
+	#FOV
+	var velocity_clamped = clamp(velocity.length(), 0.5, RUN_SPEED * 1.5)
+	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
+	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
+	
 	move_and_slide()
 
 func _sineWave(time) -> Vector3:
