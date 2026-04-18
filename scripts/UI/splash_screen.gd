a00a3125eb633cf9dfb2b97684f8bd0353361e9a
E.T <ethepaut@gaming.tech>
2026-04-18 14:35:03 +0200

    [ADD] Splah screen with button start

---
+++ scripts/UI/splash_screen.gd
@@ -0,0 +1,23 @@
+extends Control
+
+@export var scMainMenu: PackedScene
+
+var can_skip := false
+
+func _ready():
+	await get_tree().create_timer(0.5).timeout
+	can_skip = true
+
+func _input(event):
+	if not can_skip:
+		return
+
+	if event is InputEventKey and event.pressed:
+		_GoToMainMenu()
+	elif event is InputEventMouseButton and event.pressed:
+		_GoToMainMenu()
+	elif event is InputEventJoypadButton and event.pressed:
+		_GoToMainMenu()
+
+func _GoToMainMenu():
+	get_tree().change_scene_to_packed(scMainMenu)
