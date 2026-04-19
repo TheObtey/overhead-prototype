d661dbbed7e6be281796e867a98f36e8194318f2
E.T <ethepaut@gaming.tech>
2026-04-19 12:45:15 +0200

    [REF] opti de con
    Parce qu'un if est mieux que 3 pour un splash screen de merde

--- scripts/UI/SplashScreen.gd
+++ scripts/UI/SplashScreen.gd
@@ -2,23 +2,19 @@ extends Control
 
 @export var scMainMenu: PackedScene
 
-var can_skip := false
+var bCanSkip := false
 
 func _ready():
 	AudioManager.PlayMusic("res://Audio/Musics/Tests overhead.mp3")
 	
 	await get_tree().create_timer(0.5).timeout
-	can_skip = true
-	
+	bCanSkip = true
+
 func _input(event):
-	if not can_skip:
+	if not bCanSkip:
 		return
 
-	if event is InputEventKey and event.pressed:
-		_goToMainMenu()
-	elif event is InputEventMouseButton and event.pressed:
-		_goToMainMenu()
-	elif event is InputEventJoypadButton and event.pressed:
+	if event.pressed:
 		_goToMainMenu()
 
 func _goToMainMenu():
