788fa350ca95a9ecc05b984439041de5e8149221
Elia Maillard-Salin <147042560+EliaMaillard-Salin@users.noreply.github.com>
2026-04-21 13:43:52 +0200

    [REF] ADD EmoteWheel

---
+++ scripts/UI/EmoteWheel.gd
@@ -0,0 +1,61 @@
+extends Node
+
+@onready var oWheel : Control = $Wheel
+@onready var bIsOpen : bool = false
+@onready var iOpenCD : float = 0.0
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	iOpenCD += 1.0/60.0
+	if (Input.is_key_pressed(KEY_E)):
+		if bIsOpen and iOpenCD > 0.15:
+			bIsOpen = false
+			iOpenCD = 0.0
+			CloseWheel()
+		elif iOpenCD > 0.15:
+			iOpenCD = 0.0
+			bIsOpen = true
+			OpenWheel()
+	pass
+
+func EmoteInterract(iEmoteID : int) -> void :
+	CloseWheel()
+	match iEmoteID:
+		0:
+			AnimationHandler.enumNewState = AnimationHandler.AnimState.POINT
+	pass
+
+func OpenWheel() -> void:
+	oWheel.visible = true
+	pass
+
+func CloseWheel() -> void:
+	oWheel.visible = false
+	pass
+
+func OnWheelEvent(viewport: Node, event: InputEvent, shape_idx: int,iEmoteID : int) -> void :
+	var oEmoteSprite : Sprite2D = oWheel.get_child(iEmoteID)
+	if oEmoteSprite.modulate.a == 155:
+		pass
+	if event is InputEventMouseButton == false:
+		pass
+	if event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
+		CloseWheel()
+		match iEmoteID:
+			0:
+				AnimationHandler.enumNewState = AnimationHandler.AnimState.POINT
+		pass
+	pass
+	
+func OverWheelPart(iEmoteID : int) -> void :
+	var oEmoteSprite : Sprite2D = oWheel.get_child(iEmoteID)
+	oEmoteSprite.modulate.a = 210;
+	pass
+
+func NotOverWheelPart(iEmoteID : int) -> void :
+	var oEmoteSprite : Sprite2D = oWheel.get_child(iEmoteID)
+	oEmoteSprite.modulate.a = 155;
+	pass
